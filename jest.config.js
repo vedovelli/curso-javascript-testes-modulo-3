41095b5a7f06e74183f20ddbf42a402f613be629
Fabio Vedovelli <vedovelli@gmail.com>
2020-11-08 12:03:36 +0100

    Modulo3 - Aula 005 - Relatorio Code Coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
+  collectCoverageFrom: ['<rootDir>/components/**/*.js', '<rootDir>/pages/**/*.js'],
 };
