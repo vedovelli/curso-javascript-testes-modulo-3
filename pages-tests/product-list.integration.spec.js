e03cecd6e40f06e49ec5d95d7efb29b8b31dd9d7
Fabio Vedovelli <vedovelli@gmail.com>
2020-11-09 11:36:59 +0100

    Modulo3 - Aula 008 - Integration Tests - parte 4

--- pages-tests/product-list.integration.spec.js
+++ pages-tests/product-list.integration.spec.js
@@ -1,6 +1,7 @@
 import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
 import { makeServer } from '../miragejs/server';
+import Response from 'miragejs';
 
 const renderProductList = () => {
   render(<ProductList />);
@@ -33,10 +34,30 @@ describe('ProductList', () => {
     });
   });
 
-  it.todo('should render the "no products message"');
+  it('should render the "no products message"', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    });
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, '');
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument();
+      expect(screen.queryByTestId('no-products')).toBeNull();
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
+    });
+  });
+
   it.todo('should render the Search component');
   it.todo('should filter the product list when a search is performed');
-  it.todo('should display error message when promise rejects');
   it.todo('should display the total quantity of products');
   it.todo('should display product (singular) when there is only 1 product');
 });
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,22 @@ import Search from '../components/search';
 export default function Home() {
   const { products, error } = useFetchProducts();
 
+  const renderProductListOrMessage = () => {
+    if (products.length === 0 && !error) {
+      return <h4 data-testid="no-products">No products</h4>;
+    }
+    return products.map((product) => (
+      <ProductCard product={product} key={product.id} />
+    ));
+  };
+
+  const renderErrorMessage = () => {
+    if (!error) {
+      return null;
+    }
+    return <h4 data-testid="server-error">Server is down</h4>;
+  };
+
   return (
     <main data-testid="product-list" className="my-8">
       <Search />
@@ -12,9 +28,8 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
+          {renderErrorMessage()}
+          {renderProductListOrMessage()}
         </div>
       </div>
     </main>
