0fed2b911682c4583a680728c5defdc8a7e76ef3
Fabio Vedovelli <vedovelli@gmail.com>
2020-11-09 16:55:45 +0100

    Modulo3 - Aula 008 - Integration Tests - parte 8

--- components/search.js
+++ components/search.js
@@ -8,6 +8,14 @@ export default function Search({ doSearch }) {
     doSearch(term);
   };
 
+  const inputHandler = (ev) => {
+    setTerm(ev.target.value);
+
+    if (ev.target.value === '') {
+      doSearch('');
+    }
+  };
+
   return (
     <form
       onSubmit={submitHandler}
@@ -28,7 +36,7 @@ export default function Search({ doSearch }) {
 
       <input
         value={term}
-        onChange={(ev) => setTerm(ev.target.value)}
+        onInput={inputHandler}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
         type="search"
         placeholder="Search"
--- components/search.unit.spec.js
+++ components/search.unit.spec.js
@@ -5,6 +5,10 @@ import userEvent from '@testing-library/user-event';
 const doSearch = jest.fn();
 
 describe('Search', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render a form', () => {
     render(<Search doSearch={doSearch} />);
 
@@ -38,5 +42,18 @@ describe('Search', () => {
     await fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledWith(inputText);
+  });
+
+  it('should call doSearch when search input is cleared', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await userEvent.clear(input);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
+    expect(doSearch).toHaveBeenCalledWith('');
   });
 });
