25644b6b3b041414b02b6a6cc159cc3f6539fefd
Fabio Vedovelli <vedovelli@gmail.com>
2020-12-11 08:27:58 +0100

    Modulo3 - Aula 017.2 - Alterações para melhorar coverage - parte 2

--- components/cart.integration.spec.js
+++ components/cart.integration.spec.js
@@ -1,10 +1,13 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act as hooksAct } from '@testing-library/react-hooks';
 import { screen, render } from '@testing-library/react';
 import { useCartStore } from '../store/cart';
 import { makeServer } from '../miragejs/server';
 import { setAutoFreeze } from 'immer';
 import userEvent from '@testing-library/user-event';
 import Cart from './cart';
+import TestRenderer from 'react-test-renderer';
+
+const { act: componentsAct } = TestRenderer;
 
 setAutoFreeze(false);
 
@@ -36,33 +39,33 @@ describe('Cart', () => {
     expect(screen.getByTestId('cart')).toHaveClass('hidden');
   });
 
-  it('should add css class "hidden" in the component', () => {
-    act(() => {
-      toggle();
+  it('should remove css class "hidden" in the component', async () => {
+    await componentsAct(async () => {
+      render(<Cart />);
+
+      await userEvent.click(screen.getByTestId('close-button'));
+
+      expect(screen.getByTestId('cart')).not.toHaveClass('hidden');
     });
-
-    render(<Cart />);
-
-    expect(screen.getByTestId('cart')).not.toHaveClass('hidden');
   });
 
-  it('should call store toggle() twice', () => {
-    render(<Cart />);
+  it('should call store toggle() twice', async () => {
+    await componentsAct(async () => {
+      render(<Cart />);
 
-    const button = screen.getByTestId('close-button');
+      const button = screen.getByTestId('close-button');
 
-    act(() => {
-      userEvent.click(button);
-      userEvent.click(button);
+      await userEvent.click(button);
+      await userEvent.click(button);
+
+      expect(spy).toHaveBeenCalledTimes(2);
     });
-
-    expect(spy).toHaveBeenCalledTimes(2);
   });
 
   it('should display 2 products cards', () => {
     const products = server.createList('product', 2);
 
-    act(() => {
+    hooksAct(() => {
       for (const product of products) {
         add(product);
       }
