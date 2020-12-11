8d6d552fa8b6c749af04b4ca643c0258e5d73506
Fabio Vedovelli <vedovelli@gmail.com>
2020-12-11 07:54:39 +0100

    Modulo3 - Aula 017.1 - Alterações para melhorar coverage - parte 1

--- components/cart-item.unit.spec.js
+++ components/cart-item.unit.spec.js
@@ -1,5 +1,11 @@
 import { screen, render, fireEvent } from '@testing-library/react';
 import CartItem from './cart-item';
+import userEvent from '@testing-library/user-event';
+import { renderHook } from '@testing-library/react-hooks';
+import { useCartStore } from '../store/cart';
+import { setAutoFreeze } from 'immer';
+
+setAutoFreeze(false);
 
 const product = {
   title: 'Relógio bonito',
@@ -76,5 +82,20 @@ describe('CartItem', () => {
     await fireEvent.click(buttonDecrease);
 
     expect(quantity.textContent).toBe('0');
+  });
+
+  it('should call remove() when remove button is clicked', async () => {
+    const result = renderHook(() => useCartStore()).result;
+
+    const spy = jest.spyOn(result.current.actions, 'remove');
+
+    renderCartItem();
+
+    const button = screen.getByRole('button', { name: /remove/i });
+
+    await userEvent.click(button);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(product);
   });
 });
