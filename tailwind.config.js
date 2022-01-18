92d4a5dc2b57e1f95cd4b48cc5c7ecba5655bf45
Fabio Vedovelli <vedovelli@gmail.com>
2022-01-18 08:57:46 +0100

    Dependencies update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  content: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "accent-1": "#333",
-      },
-    },
+    extend: {},
   },
-  variants: {},
-  plugins: [require("@tailwindcss/ui")],
-};
+  plugins: [],
+}
